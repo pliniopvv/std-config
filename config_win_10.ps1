7fe498362f7becef6194510a1f77fe88eb40467d
pliniopvw <gangss2@hotmail.com>
2022-05-27 09:12:26 -0300

    +config_win_10.ps1

--- config_win_10.ps1
+++ config_win_10.ps1
@@ -2,6 +2,8 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 
-choco install vscode
-choco install androidstudio
-choco install slack
+choco install vscode -y
+choco install androidstudio -y
+choco install slack -y
+choco install inkscape --version 1.2 -y
+choco install gimp --version 2.10.30 -y
